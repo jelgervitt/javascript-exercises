2884bae5f09cdace308ef76e83fbc5d76cd4d0d3
Jelger <jelgervitt@gmail.com>
2021-12-30 23:08:40 -0800

    finished exercises 1 and 2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,14 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    let theString = '';
+    if (num < 0) {
+        theString = "ERROR";
+        return theString;
+    } else {
+        for (i = 0; i < num; i++) {
+            theString += string;
+        }
+    }
+    return theString;
 };
 
 // Do not edit below this line
