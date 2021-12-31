a58e92bffd7261d81d9401e573e5eb5a3352c798
Jelger <jelgervitt@gmail.com>
2021-12-31 13:31:21 -0800

    Finished exercise 7 successfully;

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,15 @@
-const ftoc = function() {
-
+function roundDecimals(theNumber, decimalPlaces) {
+	return Number(Math.round(theNumber + 'e' + decimalPlaces) + 'e-' + decimalPlaces);
 };
 
-const ctof = function() {
+const ftoc = function(tempF) {
+  let tempC = roundDecimals(((tempF - 32) * 5 / 9), 1);
+  return tempC;
+};
 
+const ctof = function(tempC) {
+  let tempF = roundDecimals((tempC * (9 / 5) + 32), 1);
+  return tempF;
 };
 
 // Do not edit below this line
