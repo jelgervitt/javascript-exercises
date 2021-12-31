1d4d91102d4c3de0b85e015723efda22a77eb49f
Jelger <jelgervitt@gmail.com>
2021-12-31 12:05:31 -0800

    Finished exercise 4 successfully

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,18 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(argumentCollection, ...removeArguments) {
+    let args = Array.from(argumentCollection);
+    let checkArgument;
+    arrayLength = args.length;
+    let outputArray = [];
+    checkToRemove: for (let i = 0; i < args.length; i++) {
+        checkArgument = args[i]
+        for (removeArgument of removeArguments) {
+            if (checkArgument === removeArgument) {
+                continue checkToRemove;
+            }
+        } 
+        outputArray.push(checkArgument);
+    }
+    return outputArray;
 };
 
 // Do not edit below this line
