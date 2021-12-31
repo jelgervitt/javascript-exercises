8dcc2a73a20274457a6fa61936882fe2744bf7e0
Jelger <jelgervitt@gmail.com>
2021-12-31 12:28:57 -0800

    Finished exercise 5 successfully

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,21 @@
-const sumAll = function() {
+const sumAll = function(firstNum, lastNum) {
+    let resultNum = 0;
+    if (firstNum < 0 || lastNum < 0 || typeof firstNum !== "number" || typeof lastNum !== "number") {
+        resultNum = "ERROR";
+        return resultNum;
+    }
 
+    if (firstNum > lastNum) {
+        start = lastNum;
+        end = firstNum;
+    } else {
+        start = firstNum;
+        end = lastNum;
+    }
+    for (let i = start; i <= end; i++) {
+        resultNum += i;
+    }
+    return resultNum;
 };
 
 // Do not edit below this line
