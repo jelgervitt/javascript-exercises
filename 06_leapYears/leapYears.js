091cea915d75a82cd470f71a7504cabf3e9826a6
Jelger <jelgervitt@gmail.com>
2021-12-31 13:00:45 -0800

    Finished exercise 6 successfully

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,6 +1,11 @@
-const leapYears = function() {
+const leapYears = function(inputYear) {
+   let outputYear;
+    if ((inputYear % 4) !== 0) outputYear = false;
+    else if ((inputYear % 4) === 0 && (inputYear % 100) !== 0) outputYear = true;
+    else if ((inputYear % 100) === 0 && (inputYear % 400) === 0) outputYear = true;
+    else outputYear = false;
+    return outputYear;
 
 };
-
 // Do not edit below this line
 module.exports = leapYears;
