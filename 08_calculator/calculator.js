f20d86a5dd65ea5593bfa917e3908d8425138319
Jelger <jelgervitt@gmail.com>
2022-01-05 23:49:04 -0800

    Finished the calculator exercise!

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,47 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+	let total = 0
+  for (part of array) {
+    total += part;
+  }
+  return total;
 };
 
-const multiply = function() {
-
+const multiply = function(factors) {
+  let total = 1
+  for (factor of factors) {
+    total *= factor;
+  }
+  return total;
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	let i = 1;
+  let total = a
+  while (i < b) {
+    total = total * a;
+    ++i;
+  }
+  return total;
 };
 
-const factorial = function() {
-	
+const factorial = function(a) {
+	if (a === 0 || a === 1) {
+    total = 1;
+  } else {
+      total = a;
+      for (let i = a ; i > 1 ; i--) {
+        total = total * (i - 1);
+    }
+  };
+  return total;
 };
 
 // Do not edit below this line
