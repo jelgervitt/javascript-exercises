7a1a2aac5b9c05b264954af95b06eaa410ebb50c
Jelger <jelgervitt@gmail.com>
2022-01-06 22:07:57 -0800

    Finished the Fibonacci exercise!

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,20 @@
-const fibonacci = function() {
-
+const fibonacci = function(num) {
+    // loop that counts the rounds
+    let previous = 0
+    let current = 1
+    let result;
+    if (Number(num) < 0) {
+        result = "OOPS"
+     } else if (Number(num) === 1) {
+         result = 1;
+     } {
+        for (i = 1 ; i < Number(num) ; ++i) {
+            result = previous + current;
+            previous = current;
+            current = result;
+        }
+    }
+    return result;
 };
 
 // Do not edit below this line
