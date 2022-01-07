c8bd107e3c9a535d480fd5ff921ebba6f27a1c87
Jelger Vitt <jelgervitt@gmail.com>
2022-01-06 18:00:26 -0800

    First attempt at exercise

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,18 @@
-const palindromes = function () {
-
+const palindromes = function (word) {
+    // create an array from the word, but without the spaces
+    let theword = [];
+    let reverseWord = [];
+    theWord = word.map(word => word.split(","));
+    // now turn the array around
+    for (letter in theWord) {
+        reverseWord.unshift(letter);
+    }
+    // now compare both arrays, will return true if palindrome
+    for (let i = 0 ; i < theWord.length ; i++) {
+        (theWord[i] === reverseWord[i])
+    };
 };
+
 
 // Do not edit below this line
 module.exports = palindromes;
